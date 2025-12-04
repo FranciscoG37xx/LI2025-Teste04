5c0cae4d00819002f08bbdbe56a07208dc0199d7
FranciscoG37xx <francisco52garrido@gmail.com>
2025-12-04 15:04:39 +0000

    5ª tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-"//ficheiro de script" 
+// ficheiro de script — validação Bootstrap
+
+(() => {
+    'use strict';
+
+    const form = document.querySelector('.needs-validation');
+
+    form.addEventListener('submit', event => {
+
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+    });
+
+})();
+
